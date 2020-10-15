e0971a5a621da02d35c07bdce3b930ec4edcf957
akkaradet13 <akkaradet.pu.60@ubu.ac.th>
2020-10-15 16:18:15 +0700

    2 files modified

--- src/app/find-cameraman/find-cameraman.component.ts
+++ src/app/find-cameraman/find-cameraman.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-find-cameraman',
-  templateUrl: './find-cameraman.component.html',
-  styleUrls: ['./find-cameraman.component.css']
+  selector: "app-find-cameraman",
+  templateUrl: "./find-cameraman.component.html",
+  styleUrls: ["./find-cameraman.component.css"]
 })
 export class FindCameramanComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afs: AngularFirestore) {
+    this.afs
+      .collection("items")
+      .valueChanges()
+      .subscribe(vals => {
+        vals.map(val => {
+          console.log("val => ", val["number"]);
+        });
+      });
   }
 
+  ngOnInit() {}
 }
