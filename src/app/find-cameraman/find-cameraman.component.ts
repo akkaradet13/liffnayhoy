cf95343d28469a2f30250dfed1688d0a106e109d
akkaradet13 <akkaradet.pu.60@ubu.ac.th>
2020-10-15 17:19:16 +0700

    2 files modified

--- src/app/find-cameraman/find-cameraman.component.ts
+++ src/app/find-cameraman/find-cameraman.component.ts
@@ -10,11 +10,13 @@ import {
   styleUrls: ["./find-cameraman.component.css"]
 })
 export class FindCameramanComponent implements OnInit {
+  data;
   constructor(private afs: AngularFirestore) {
     this.afs
       .collection("items")
       .valueChanges()
       .subscribe(vals => {
+        this.data = vals;
         vals.map(val => {
           console.log("val => ", val["number"]);
         });
